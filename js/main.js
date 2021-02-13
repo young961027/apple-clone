022052b6b9151632518af546fdfa1f11dce9d794
loopbackseal <young961027@gmail.com>
2021-02-13 19:55:13 +0900

    Scroll Animation 3. main-message Opacity control

--- js/main.js
+++ js/main.js
@@ -70,7 +70,12 @@
     }
 
     function calcValues(values, currentYOffset) {
-        
+        let rv;
+        // 현재 씬(스크롤 섹션)에서 스크롤 된 범위를 비율로 구하기
+        let scrollRatio = currentYOffset / sceneInfo[currentScene].scrollHeight;
+        rv = scrollRatio * (values[1] - values[0]) + values[0];
+        console.log(rv);
+        return rv;
     }
 
     function playAnimation() {
@@ -81,10 +86,8 @@
         switch (currentScene) {
             case 0:
                 // console.log("0 play");
-                let messageA_opacity_0 = values.messageA_opacity[0];
-                let messageA_opacity_1 = values.messageA_opacity[1];
-
-                console.log(calcValues(values.messageA_opacity,currentYOffset));
+                let messageA_opacity_in = calcValues(values.messageA_opacity, currentYOffset);
+                objs.messageA.style.opacity = messageA_opacity_in;
                 break;
             case 1:
                 // console.log("1 play");
