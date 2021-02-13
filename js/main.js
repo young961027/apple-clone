4629d1f4604d6b7403421038b53bb70fbc981114
loopbackseal <young961027@gmail.com>
2021-02-13 19:04:54 +0900

    Scroll Animation 1

--- js/main.js
+++ js/main.js
@@ -6,7 +6,14 @@
             heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 세팅
             scrollHeight: 0,
             objs: {
-                container: document.querySelector('#scroll-section-0')
+                container: document.querySelector('#scroll-section-0'),
+                messageA: document.querySelector('#scroll-section-0 .main-message.a'),
+                messageB: document.querySelector('#scroll-section-0 .main-message.b'),
+                messageC: document.querySelector('#scroll-section-0 .main-message.c'),
+                messageD: document.querySelector('#scroll-section-0 .main-message.d')
+            },
+            values: {
+                messageA_opacity: [0,1]
             }
         },
         {
@@ -37,7 +44,11 @@
             }
         }
     ];
-
+    
+    let yOffset = 0; // window.pageYOffset 대신 쓸 변수
+    let prevScrollHeight = 0; // 현재 스크롤 위치(yOffset)보다 이전에 위차한 스크롤 섹션들의 스크롤 높이의 합
+    let currentScene = 0; // 현재 활성화된(눈 앞에 보고있는) 씬(scroll-section)
+    
     function setLayout() {
         // 각 스크롤 섹션의 높이 세팅
         for (let i=0; i <sceneInfo.length; i++) {
@@ -58,9 +69,22 @@
         document.body.setAttribute('id', `show-scene-${currentScene}`);
     }
 
-    let yOffset = 0; // window.pageYOffset 대신 쓸 변수
-    let prevScrollHeight = 0; // 현재 스크롤 위치(yOffset)보다 이전에 위차한 스크롤 섹션들의 스크롤 높이의 합
-    let currentScene = 0; // 현재 활성화된(눈 앞에 보고있는) 씬(scroll-section)
+    function playAnimation() {
+        switch (currentScene) {
+            case 0:
+                // console.log("0 play");
+                break;
+            case 1:
+                // console.log("1 play");
+                break;
+            case 2:
+                // console.log("2 play");
+                break;
+            case 3:
+                // console.log("3 play");
+                break;
+        }
+    }
 
     function scrollLoop() {
         prevScrollHeight = 0;
@@ -70,16 +94,16 @@
 
         if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
             currentScene++;
-            // document.body.setAttribute('id', `show-scene-${currentScene}`);
+            document.body.setAttribute('id', `show-scene-${currentScene}`);
         }
 
         if (yOffset < prevScrollHeight) {
             if (currentScene === 0) return; // 브라우저 바운스 효과로 currentScene이 음수가 되는 것을 방지.
-            // document.body.setAttribute('id', `show-scene-${currentScene}`);
             currentScene--;
+            document.body.setAttribute('id', `show-scene-${currentScene}`);
         }
 
-        document.body.setAttribute('id', `show-scene-${currentScene}`);
+        playAnimation();
     }
 
     window.addEventListener('scroll', () => {
