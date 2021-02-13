790b82297009f64122f390f8ede10f1b98b0f1e0
loopbackseal <young961027@gmail.com>
2021-02-14 00:14:27 +0900

    Scroll Animation - section0 complete

--- js/main.js
+++ js/main.js
@@ -15,10 +15,21 @@
             values: {
                 messageA_opacity_in: [0,1, {start: 0.1, end: 0.2}],
                 messageA_translateY_in: [20, 0, {start: 0.1, end: 0.2}],
-                // messageB_opacity_in: [0,1, {start: 0.3, end: 0.4}],
+                messageB_opacity_in: [0,1, {start: 0.3, end: 0.4}],
+                messageB_translateY_in: [20,0, {start: 0.3, end: 0.4}],
+                messageC_opacity_in: [0,1, {start: 0.5, end: 0.6}],
+                messageC_translateY_in: [20,0, {start: 0.5, end: 0.6}],
+                messageD_opacity_in: [0,1, {start: 0.7, end: 0.8}],
+                messageD_translateY_in: [20,0, {start: 0.7, end: 0.8}],
                 
                 messageA_opacity_out: [1,0, {start: 0.25, end: 0.3}],
-                messageA_translateY_out: [0, 20, {start: 0.25, end: 0.3}],
+                messageA_translateY_out: [0, -20, {start: 0.25, end: 0.3}],
+                messageB_opacity_out: [1,0, {start: 0.45, end: 0.5}],
+                messageB_translateY_out: [0, -20, {start: 0.45, end: 0.5}],
+                messageC_opacity_out: [1,0, {start: 0.65, end: 0.7}],
+                messageC_translateY_out: [0, -20, {start: 0.65, end: 0.7}],
+                messageD_opacity_out: [1,0, {start: 0.85, end: 0.9}],
+                messageD_translateY_out: [0, -20, {start: 0.85, end: 0.9}],
             }
         },
         {
@@ -58,7 +69,11 @@
     function setLayout() {
         // 각 스크롤 섹션의 높이 세팅
         for (let i=0; i <sceneInfo.length; i++) {
-            sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
+            if (sceneInfo[i].type === 'sticky') {
+                sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
+            } else if (sceneInfo[i].type === 'normal') {
+                sceneInfo[i].scrollHeight = sceneInfo[i].objs.container.offsetHeight;
+            }
             sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
         }
 
@@ -110,22 +125,38 @@
         switch (currentScene) {
             case 0:
                 // console.log("0 play");
-                const messageA_opacity_in = calcValues(values.messageA_opacity_in, currentYOffset);
-                const messageA_opacity_out = calcValues(values.messageA_opacity_out, currentYOffset);
-
-                const messageA_translateY_in = calcValues(values.messageA_translateY_in, currentYOffset);
-                const messageA_translateY_out = calcValues(values.messageA_translateY_out, currentYOffset);
-                
                 if (scrollRatio <= 0.22) {
-                    objs.messageA.style.opacity = messageA_opacity_in;
-                    objs.messageA.style.transform = `translateY(${messageA_translateY_in}%)`;
+                    objs.messageA.style.opacity = calcValues(values.messageA_opacity_in, currentYOffset);
+                    objs.messageA.style.transform = `translateY(${calcValues(values.messageA_translateY_in, currentYOffset)}%)`;
                 } else {
-                    objs.messageA.style.opacity = messageA_opacity_out;
-                    objs.messageA.style.transform = `translateY(${messageA_translateY_out}%)`;
+                    objs.messageA.style.opacity = calcValues(values.messageA_opacity_out, currentYOffset);
+                    objs.messageA.style.transform = `translateY(${calcValues(values.messageA_translateY_out, currentYOffset)}%)`;
+                }
+                if (scrollRatio <= 0.42) {
+                    objs.messageB.style.opacity = calcValues(values.messageB_opacity_in, currentYOffset);
+                    objs.messageB.style.transform = `translateY(${calcValues(values.messageB_translateY_in, currentYOffset)}%)`;
+                } else {
+                    objs.messageB.style.opacity = calcValues(values.messageB_opacity_out, currentYOffset);
+                    objs.messageB.style.transform = `translateY(${calcValues(values.messageB_translateY_out, currentYOffset)}%)`;
+                }
+                if (scrollRatio <= 0.62) {
+                    objs.messageC.style.opacity = calcValues(values.messageC_opacity_in, currentYOffset);
+                    objs.messageC.style.transform = `translateY(${calcValues(values.messageC_translateY_in, currentYOffset)}%)`;
+                } else {
+                    objs.messageC.style.opacity = calcValues(values.messageC_opacity_out, currentYOffset);
+                    objs.messageC.style.transform = `translateY(${calcValues(values.messageC_translateY_out, currentYOffset)}%)`;
+                }
+                if (scrollRatio <= 0.82) {
+                    objs.messageD.style.opacity = calcValues(values.messageD_opacity_in, currentYOffset);
+                    objs.messageD.style.transform = `translateY(${calcValues(values.messageD_translateY_in, currentYOffset)}%)`;
+                } else {
+                    objs.messageD.style.opacity = calcValues(values.messageD_opacity_out, currentYOffset);
+                    objs.messageD.style.transform = `translateY(${calcValues(values.messageD_translateY_out, currentYOffset)}%)`;
                 }
                 break;
             case 1:
                 // console.log("1 play");
+
                 break;
             case 2:
                 // console.log("2 play");
