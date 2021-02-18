2eedf1ede82814f5093e09e9b864356d9c8b4f40
loopbackseal <young961027@gmail.com>
2021-02-18 15:12:09 +0900

    Section 03. Fix canvas after Animation

--- js/main.js
+++ js/main.js
@@ -374,6 +374,15 @@
                     objs.canvas.height
                 );
 
+                if (scrollRatio < values.rect1X[2].end) {
+                    step = 1;
+                    objs.canvas.classList.remove("sticky");
+                } else {
+                    step = 2;
+                    objs.canvas.classList.add("sticky");
+                    objs.canvas.style.top = `${0-(objs.canvas.height - objs.canvas.height * canvasScaleRatio)/2}px`;
+                }
+
                 break;
         }
     }
